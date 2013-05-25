633aedbc6789e1eea873ec6716f72763f4dff450
Alan <am3692@ucla.edu>
2013-05-25 16:10:35 -0700

    Added pedometer functionality, replacing the pure acceleration data

--- naviDration/src/com/example/navidration/MainActivity.java
+++ naviDration/src/com/example/navidration/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.navidration;
 
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -18,11 +19,8 @@ import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Timer;
 import java.util.concurrent.TimeUnit;
+import android.opengl.*;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -30,17 +28,38 @@ import android.graphics.*;
 import com.google.android.maps.*;
 
 
-public class MainActivity extends MapActivity implements SensorEventListener {
-	  private SensorManager mSensorManager;
-	  private Sensor mAccelerometer;
-	  TextView title,tv,tv1,tv2, tv3;
-	  RelativeLayout layout;
-	  private FileWriter writer;
-	  private List<Overlay> mapOverlays;
-	  private MapView mapView;
-	  private Projection projection;  
-	  String weatherData;
-	  DecimalFormat dForm;
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mGravity;
+    private Sensor mMagnetic;
+    TextView title,tv3, tv4;
+    RelativeLayout layout;
+    private FileWriter writer;
+
+    float[] gravity = null;
+    float[] magnet = null;
+    float[] inR = new float[16];
+    float[] outR = new float[16];
+    float[] output = new float[4];
+    float[] linAcceleration = {0,0,0,0};
+    private float localMin = 0;
+    private float localMax = 0;
+    private float localMean = 0;
+    private float threshold = 0;
+    private int step = 0;
+
+
+
+
+    private List<Overlay> mapOverlays;
+    private MapView mapView;
+    private Projection projection;
+
+
+    String weatherData;
+    DecimalFormat dForm = new DecimalFormat("#.###");
+    boolean weatherDataRetrieved;
 
 	  private class WeatherAsyncTask extends AsyncTask<Void, Integer, Void>{
 	      
@@ -48,6 +67,7 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 			protected void onPreExecute() {
 			// update the UI immediately after the task is executed
 				super.onPreExecute();
+                weatherDataRetrieved = false;
 
 			}
 
@@ -59,6 +79,7 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 			@Override
 			protected void onPostExecute(Void result) {
 				super.onPostExecute(result);
+                weatherDataRetrieved = true;
 			}
 
 			@Override
@@ -82,6 +103,7 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 					  	if(urlConnection!=null){
 					  		urlConnection.setDoInput(true);
 					  		}
+
 					  	InputStream is = urlConnection.getInputStream();
 					  	InputStreamReader isr = new InputStreamReader(is);
 			            in = new BufferedReader(isr);
@@ -89,12 +111,13 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 
 			                JSONObject weather_json = new JSONObject(l);
 			                
-			                String tempC = weather_json.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getString("temp_C");
+			                String tempF = weather_json.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getString("temp_F");
 			                String humidity = weather_json.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getString("humidity");
-			                System.out.println("Weather tempC: "+tempC);
+			                System.out.println("Weather tempC: "+tempF);
 			                System.out.println("Weather humidity: "+humidity);
-			                weatherData = tempC+","+humidity;
+			                weatherData = tempF+","+humidity;
 			            }
+                        weatherDataRetrieved = true;
 			            in.close();
 			        }
 				  catch (Exception ex)
@@ -121,82 +144,37 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 	  public final void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_main);
-	    dForm = new DecimalFormat("#.###");
+          System.out.println("Creating!");
 	    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 	    mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mGravity= mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+	    mMagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-	    
 	    //get layout
-	    layout = (RelativeLayout)findViewById(R.id.relative);
-	    mapView = (MapView) findViewById(R.id.mapview);
-	    mapView.setBuiltInZoomControls(true);
-
-	    mapOverlays = mapView.getOverlays();        
-	    projection = mapView.getProjection();
-	    mapOverlays.add(new MyOverlay());    
 	    WeatherAsyncTask task = new WeatherAsyncTask();
 	    task.execute();
-	    try
-	    {
-	    task.get(1000, TimeUnit.MILLISECONDS);
-	    }
-	    catch (Exception ex)
-	    {
-	    	
-	    }
-	    System.out.println("WeatherData: "+weatherData);
+        while (!weatherDataRetrieved)
+          {
+            try
+            {
+            task.get(1000, TimeUnit.MILLISECONDS);
+            }
+            catch (Exception ex)
+            {
+
+            }
+          }
 	    //get textviews
-	    title=(TextView)findViewById(R.id.name);   
-	    tv=(TextView)findViewById(R.id.xval);
-	    tv1=(TextView)findViewById(R.id.yval);
-	    tv2=(TextView)findViewById(R.id.zval);
+        System.out.println("Got weather data!");
+	    title=(TextView)findViewById(R.id.name);
 	    tv3=(TextView)findViewById(R.id.weatherval);
+        tv4 = (TextView)findViewById(R.id.stepcount);
+        title.setText(R.string.app_name);
 	    tv3.setText(weatherData);
-	    Timer timer = new Timer();	
+
 	    
 	  }
 
-	  @Override
-		protected boolean isRouteDisplayed() {
-		    return false;
-		}
-	 
-
-		class MyOverlay extends Overlay{
-
-		    public MyOverlay(){
-
-		    }   
-
-		    public void draw(Canvas canvas, MapView mapv, boolean shadow){
-		        super.draw(canvas, mapv, shadow);
-
-		        Paint   mPaint = new Paint();
-		        mPaint.setDither(true);
-		        mPaint.setColor(Color.RED);
-		        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-		        mPaint.setStrokeJoin(Paint.Join.ROUND);
-		        mPaint.setStrokeCap(Paint.Cap.ROUND);
-		        mPaint.setStrokeWidth(2);
-
-		        GeoPoint gP1 = new GeoPoint(34059699,-118444934);
-		        GeoPoint gP2 = new GeoPoint(34064641, -118445406);
-
-		        Point p1 = new Point();
-		        Point p2 = new Point();
-		        Path path = new Path();
-
-		        projection.toPixels(gP1, p1);
-		        projection.toPixels(gP2, p2);
-
-		        path.moveTo(p2.x, p2.y);
-		        path.lineTo(p1.x,p1.y);
-
-		        canvas.drawPath(path, mPaint);
-		    }
-		
-		
-		}
 	  @Override
 	  public final void onAccuracyChanged(Sensor sensor, int accuracy)
 	  {
@@ -206,11 +184,16 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 	  @Override
 	  public final void onSensorChanged(SensorEvent event) 
 	  {
-		  
-		  
-	    float x =  event.values[0];
-	    float y =  event.values[1];
-	    float z =  event.values[2];
+          if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
+              gravity = event.values;
+          }
+          if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+              magnet = event.values;
+          }
+          if (gravity != null && magnet != null && event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+              processAccelerationData(event.values);
+          }
+        /*
 	    try
 	    {
 	    	writer.write(""+x+","+y+","+z);
@@ -219,19 +202,66 @@ public class MainActivity extends MapActivity implements SensorEventListener {
 	    catch (Exception ex)
 	    {
 	    }
-	    //display values using TextView
-	    title.setText(R.string.app_name);
-	    tv.setText("X axis" +"\t\t"+x);
-	    tv1.setText("Y axis" + "\t\t" +y);
-	    tv2.setText("Z axis" +"\t\t" +z);
+	    */
 	    
 	  }
+
+    protected void processAccelerationData(float[] data)
+    {
+
+        linAcceleration[0]= data[0];
+        linAcceleration[1] = data[1];
+        linAcceleration[2] = data[2];
+        System.out.print("Before matrices");
+        SensorManager.getRotationMatrix(inR, null, gravity, magnet);
+        System.out.print("1 matrices");
+        android.opengl.Matrix.invertM(outR, 0, inR, 0);
+        System.out.print("2 matrices");
+        if (0.9 < outR[6] && outR[6] < 1.1 ) {
+            System.out.print("Returned!");
+            return;
+        }
+        float[] result = new float[4];
+        System.out.print("3 matrices");
+        android.opengl.Matrix.multiplyMV(result, 0, outR, 0, linAcceleration, 0);
+        System.out.print("4 matrices");
+        float xyAcceleration = android.util.FloatMath.sqrt(result[0]*result[0] + result[1]*result[1]);
+
+        if (xyAcceleration > 1.3) {
+            if (localMax < result[2]) {
+                localMax = result[2];
+            }
+            if (localMin > result[2] && result[2] < localMax) {
+                localMin = result[2];
+            }
+            localMean = (localMax + localMin) / 2;
+            if (localMin <= localMean && localMean <= localMax) {
+                threshold++;
+                localMax = 0;
+                localMin = 0;
+
+            }
+            if (threshold > 50) {
+                threshold = 0;
+                step++;
+            }
+        }
+
+        //display values using TextView
+
+        //tv.setText("X axis" +"\t\t"+linAcceleration[0]);
+        //tv1.setText("Y axis" + "\t\t" +linAcceleration[1]);
+        //tv2.setText("Z axis" +"\t\t" +linAcceleration[2]);
+        tv4.setText("Step count " +"\t\t" +step);
+    }
 
 	  @Override
 	  protected void onResume()
 	  {
 	    super.onResume();
-	    mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	    mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mMagnetic, SensorManager.SENSOR_DELAY_FASTEST);
 	    try
 	    {
 	    	File writeTo = new File(android.os.Environment.getExternalStorageDirectory(), "accelerometer_data.txt");
