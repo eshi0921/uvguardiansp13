409002533021d5eb09c5cb94cb5f2de65c9722a8
Alan <am3692@ucla.edu>
2013-06-06 17:48:03 -0700

    Update PHP code to access database

--- naviDration/Database/addWaterFountain.php
+++ naviDration/Database/addWaterFountain.php
@@ -10,12 +10,16 @@
 		die('Invalid input! Password');
 	}
 	
-	$longitude = $_GET['long'];
-	$latitude = $_GET['lat'];
+	$longitude = $_GET['longitude'];
+	$latitude = $_GET['latitude'];
+	$rating = $_GET['rating'];
+	$uid = $_GET['uid'];
 	
+	if (!($rating == 'Y' OR $rating == 'N')) {
+		die('Invalid input! Rating');
+	}
 	
 	$sql = 'INSERT INTO Fountain_Location (Longitude, Latitude) VALUES ('.$longitude.','.$latitude.");" ;
-
 
 	$con = new mysqli($host, $user, $pass, $dbase);
 	if($con->connect_errno > 0){
@@ -25,8 +29,41 @@
 	if(!$result = $con->query($sql)){
 			die('There was an error running the query [' . $con->error . ']');
 		}
+		
+		
+	$sql = 'SELECT * FROM Fountain_Location WHERE Longitude='.$longitude.' AND Latitude='.$latitude.';' ;
+	if(!$result = $con->query($sql)){
+			die('There was an error running the query [' . $con->error . ']');
+		}
 
-	echo "Success";
+	$row = $result->fetch_assoc();
+	$fid = $row['Fount_ID'];
+	
+		
+	$sql = 'INSERT INTO Fountain_Rating (Fount_ID, Rating, User_ID) VALUES ('.$fid.',\''.$rating.'\','.$uid.");" ;
+	if(!$result = $con->query($sql)){
+			die('There was an error running the query [' . $con->error . ']');
+		}
+
+	$sqlY = 'SELECT Count(Rating) AS numYes FROM Fountain_Rating WHERE Fount_ID='.$fid.' AND Rating=\'Y\';';
+	$sqlN = 'SELECT Count(Rating) AS numNo FROM Fountain_Rating WHERE Fount_ID='.$fid.' AND Rating=\'N\';';
+
+	if(!$resultY = $con->query($sqlY)){
+	die('There was an error running the query [' . $con->error . ']');
+	}
+
+	if(!$resultN = $con->query($sqlN)){
+	die('There was an error running the query [' . $con->error . ']');
+	}
+
+	$rowY = $resultY->fetch_assoc();
+	$numY = $rowY['numYes'];
+
+	$rowN = $resultN->fetch_assoc();
+	$numN = $rowN['numNo'];
+
+	echo $fid.','.$latitude.','.$longitude.','.$numY.','.$numN;	
+	
 			
 ?>
 	
